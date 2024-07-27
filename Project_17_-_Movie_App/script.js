ba751e47dcd2db6f2907fd69635288e5ee58e9a7
Kristina Palmquist <kristina.palmquist@gmail.com>
2024-07-27 13:23:05 +0200

    Updates to Project 17

--- Project_17_-_Movie_App/script.js
+++ Project_17_-_Movie_App/script.js
@@ -2,8 +2,20 @@ const API_URL =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5ea5f89a2f3da65b1662a18e200a1446&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
-const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=5ea5f89a2f3da65b1662a18e200a1446&query="';
+let key = '';
+
+fetch('./assets/apiKey.json')
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => (key = data.api_key))
+  .catch((error) => console.error('Unable to fetch data:', error));
+
+const SEARCH_API = `
+  https://api.themoviedb.org/3/search/movie?api_key=${key}"`;
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
